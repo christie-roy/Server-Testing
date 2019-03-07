d26b7bea522b4047a4f839dade22b5e59f4a4dc6
croy <roy.christie95@gmail.com>
2019-03-07 16:34:51 -0600

    Added POST and DELETE tests and endpoints

--- server/server.js
+++ server/server.js
@@ -8,4 +8,16 @@ server.get('/', async (req, res) => {
     res.status(200).json({ msg: 'running!'});
 });
 
+server.get('/resource', async (req, res) => {
+    res.status(200).json({ msg: 'running!'});
+});
+
+server.post('/resource', async (req, res) => {
+    res.status(201).json({ msg: 'added'});
+})
+
+server.delete('/resource/:id', async (req, res) => {
+    res.status(200).json({ msg: 'deleted!'});
+});
+
 module.exports = server;
--- server/server.spec.js
+++ server/server.spec.js
@@ -25,5 +25,66 @@ describe('GET /', () => {
     it('should return a msg', async () => {
         const res = await request(server).get('/');
         expect(res.body).toEqual({ msg: 'running!'});
-    })
+    });
+
 })
+
+describe('GET /resource', () => {
+    it('should return 200', () => {
+        return request(server)
+        .get('/resource')
+        .then(res => {
+            expect(res.status).toBe(200);
+        });
+    });
+
+    it('should return JSON', async () => {
+        const res = await request(server).get('/resource');
+        expect(res.type).toBe('application/json');
+    })
+
+    it('should return a msg', async () => {
+        const res = await request(server).get('/resource');
+        expect(res.body).toEqual({ msg: 'running!'});
+    })
+});
+
+describe('POST /resource', () => {
+    it('should return 201', () => {
+        return request(server)
+        .post('/resource')
+        .then(res => {
+            expect(res.status).toBe(201);
+        });
+    });
+
+    it('should return JSON', async () => {
+        const res = await request(server).get('/resource');
+        expect(res.type).toBe('application/json');
+    })
+
+    it('should return a msg', async () => {
+        const res = await request(server).post('/resource');
+        expect(res.body).toEqual({ msg: 'added'});
+    })
+});
+
+describe('DELETE /resource/:id', () => {
+    it('should return 200', () => {
+        return request(server)
+        .delete('/resource/:id')
+        .then(res => {
+            expect(res.status).toBe(200);
+        });
+    });
+
+    it('should return JSON', async () => {
+        const res = await request(server).delete('/resource/:id');
+        expect(res.type).toBe('application/json');
+    })
+
+    it('should return a msg', async () => {
+        const res = await request(server).delete('/resource/:id');
+        expect(res.body).toEqual({ msg: 'deleted!'});
+    })
+});
