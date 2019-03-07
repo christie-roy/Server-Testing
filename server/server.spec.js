e7aaec35f5c86aa8cd511c97337207eb0a92dada
croy <roy.christie95@gmail.com>
2019-03-07 16:23:14 -0600

    Completed three server tests

--- server/server.spec.js
+++ server/server.spec.js
@@ -3,13 +3,27 @@ const request = require('supertest');
 const server = require('./server.js');
 
 describe('server.js', () => {
-    expect(process.env.DB_ENV).toBe('testing');
+    it('should be a testing env', () => {
+        expect(process.env.DB_ENV).toBe('testing');
+    })
 });
 
 describe('GET /', () => {
-    return request(server)
+    it('should return 200', () => {
+        return request(server)
         .get('/')
         .then(res => {
             expect(res.status).toBe(200);
         });
+    });
+
+    it('should return JSON', async () => {
+        const res = await request(server).get('/');
+        expect(res.type).toBe('application/json');
+    })
+
+    it('should return a msg', async () => {
+        const res = await request(server).get('/');
+        expect(res.body).toEqual({ msg: 'running!'});
+    })
 })
